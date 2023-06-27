9944030ad67f972128d2ac11fa6dd94cba78f208
newt pc work <axelskam.123@outlook.es>
2023-06-27 18:04:18 -0500

    add ebosadora field

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import json
 
-class RowData:
-  def __init__(self, Nom, Dep, Dis, Dir, Est, Hor, Sab):
+class Agency:
+  def __init__(self, Nom, Dep, Dis, Dir, Est, Hor, Sab, Emb):
     self.Nom = Nom
     self.Dep = Dep
     self.Dis = Dis
@@ -10,11 +10,12 @@ class RowData:
     self.Est = Est
     self.Hor = Hor
     self.Sab = Sab
-    # self.Emb = Emb
+    self.Emb = Emb
 
 df = pd.read_excel("./agencias.xlsx", skiprows=5)
 
 column_names = df.columns
+column_name_embosadora = column_names[column_names.str.contains("Embozadora")].tolist()
 
 index_agencia = column_names.get_loc("AGENCIA")
 index_provincia = column_names.get_loc("PROVINCIAS")
@@ -23,14 +24,15 @@ index_direccion = column_names.get_loc("DIRECCIÓN")
 index_estado = column_names.get_loc("Estado")
 index_hor = column_names.get_loc("HR Lun-Vie")
 index_hor_sab = column_names.get_loc("HR Sab")
+index_embosadora = column_names.get_loc(column_name_embosadora[0])
 
-list_agencias = []
+list_agencies = []
 
 for row in df.values.tolist():
-  agencia = RowData(row[index_agencia], row[index_provincia], row[index_distrito], row[index_direccion], row[index_estado], row[index_hor], row[index_hor_sab])
-  list_agencias.append(agencia)
+  agency = Agency(row[index_agencia], row[index_provincia], row[index_distrito], row[index_direccion], row[index_estado], row[index_hor], row[index_hor_sab], row[index_embosadora])
+  list_agencies.append(agency)
 
-final_json = json.dumps([a.__dict__ for a in list_agencias], ensure_ascii=False)
+final_json = json.dumps([a.__dict__ for a in list_agencies], ensure_ascii=False, indent=1)
 
 with open('data.json', 'w', encoding='utf-8') as f:
   f.write(final_json)
